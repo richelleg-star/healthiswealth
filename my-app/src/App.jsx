ebae46c3371a1e1b23ecf161e6bf127bb9187cd0
richelleg-star <rgust2@uw.edu>
2026-03-12 10:37:38 -0700

    added more pages and functionality

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Routes, Route, Outlet, Navigate } from 'react-router';
 import { Homepage } from './pages/homepage';
+import { HealthEvents } from './pages/healthevents';
 import { ref, onValue, getDatabase} from "firebase/database"
+import {ProviderLoginPage} from "./pages/providerlogin"
 
 export default function App() {
   return (
     <Routes>
       <Route path="*" element={<Homepage/>} />
+      <Route path="healthevents" element={<HealthEvents/>} />
+      <Route path="providerlogin" element={<ProviderLoginPage/>} />
       {/* </Routes><Route path="healthevents" element={<health>} /> */}
     </Routes>
   )
--- my-app/src/pages/healthevents.jsx
+++ my-app/src/pages/healthevents.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { SearchFunction } from "../components/searchfunction";
+import { Link, useNavigate, NavLink } from "react-router";
+import { BrowseCards } from "../cards/regularcards";
+import { ViewMap } from "../components/mapintegration";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { LoggedOutProviderBar } from '../navbar/notproviderbar';
+import { EventCards } from "../cards/eventcards";
+
 
 export function HealthEvents(){
+    const filtereditems1 = ['Any Date', 'Today', 'This Weekend'];
+    const filtereditems2 = ['All Events', 'Vaccine Drives', 'Health Screenings'];
     return(
-        <div id="events-view" class="view-section">
-            <section class="hero">
+        <>
+        <header>
+            <LoggedOutProviderBar/>
+        </header>
+        
+        <div id="events-view" className="view-section">
+            <section className="hero">
                 <h1>Community Health Events</h1>
                 <p>Find free pop-up clinics, health fairs, and vaccination drives happening this week.</p>
             </section>
+            
         </div>
+        <SearchFunction filteritems1={filtereditems1} filteritems2={filtereditems2}/>
+        <main className="container">
+                <div>
+                    <div className="list-view">
+                        <EventCards/>
+                        <EventCards/>
+                        <EventCards/>
+                    </div>
+                </div>
+                <ViewMap/>
+            </main>
+        </>
     )
 }
